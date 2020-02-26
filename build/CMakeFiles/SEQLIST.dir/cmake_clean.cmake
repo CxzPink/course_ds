0541fd730c17b9403f04a123580820bbb22babd9
cxz <pink.tortoiser@gmail.com>
2020-02-27 00:21:27 +0800

    first homework

---
+++ build/CMakeFiles/SEQLIST.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SEQLIST.dir/src/main.cpp.o"
+  "CMakeFiles/SEQLIST.dir/src/seqlist.cpp.o"
+  "../bin/SEQLIST.pdb"
+  "../bin/SEQLIST"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SEQLIST.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
